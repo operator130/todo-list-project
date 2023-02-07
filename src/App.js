0cd7a3caff23e12ecc6a1c12e9255863da05f076
Bo Bartolome <bo.bartolome@proton.me>
2023-02-06 19:15:43 -1000

    add all necessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TodoList from './TodoList';
+import { HashRouter, Route, Link } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  return(
+    <HashRouter basename='/'>
+      <div className="">
+        <TodoList />
+      </div>
+    </HashRouter>
   );
-}
+  }
+
+const TodoList = () => <div><h2>TodoList</h2></div>
 
 export default App;
